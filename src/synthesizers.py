4a2ecb23bbfae2676f2677cc80a42df3be172e70
Jean Nassar <jean.nassar@yields.io>
2020-06-21 19:15:10 +0200

    Rename voice to midi_channel.

--- src/synthesizers.py
+++ src/synthesizers.py
@@ -15,7 +15,7 @@ class SynthVelocities:
 
 @dc.dataclass
 class Midi:
-    voice: int = dc.field(repr=False)
+    midi_channel: int = dc.field(repr=False)
     note_number: int
     velocity: int
 
@@ -31,10 +31,10 @@ class Synthesizer(abc.ABC):
 
 
 class MidiSynthesizer(Synthesizer):
-    def __init__(self, note_number: int = 60, velocities: SynthVelocities = SynthVelocities(), voice: int = 0):
+    def __init__(self, note_number: int = 60, velocities: SynthVelocities = SynthVelocities(), midi_channel: int = 0):
         self.note_number = note_number
         self.velocities = velocities
-        self.voice = voice
+        self.midi_channel = midi_channel
 
     def play_pattern(self, pattern: Pattern) -> Generator[Midi, None, None]:
         pattern_prev, pattern_curr = it.tee(pattern)
@@ -46,7 +46,7 @@ class MidiSynthesizer(Synthesizer):
     def play_note(self, note: Note, follows_double: bool = False) -> Generator[Midi, None, None]:
         if note.is_single:
             velocity = self.velocities.accent if follows_double else self.velocities.normal
-            yield Midi(voice=self.voice, note_number=self.note_number, velocity=velocity)
+            yield Midi(midi_channel=self.midi_channel, note_number=self.note_number, velocity=velocity)
         elif note.is_double:
-            yield Midi(voice=self.voice, note_number=self.note_number, velocity=self.velocities.normal)
-            yield Midi(voice=self.voice, note_number=self.note_number, velocity=self.velocities.soft)
+            yield Midi(midi_channel=self.midi_channel, note_number=self.note_number, velocity=self.velocities.normal)
+            yield Midi(midi_channel=self.midi_channel, note_number=self.note_number, velocity=self.velocities.soft)
