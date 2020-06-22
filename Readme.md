1e450c2158a7eb9c3382df32646cfc13c933e8fd
Jean Nassar <jean.nassar@yields.io>
2020-06-22 23:35:49 +0200

    Add setup boilerplate.

---
+++ Readme.md
@@ -0,0 +1,1 @@
+Automatically generate and synthesize Jaki music.
--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
+from pathlib import Path
 from setuptools import setup
+
+
+with Path("requirements.txt").open() as requirements:
+    DEPENDENCIES = requirements.readlines()
+
 
 setup(
     name='autojaki',
     version='0.0.1',
     description='Automatically generate and use Jaki codes.',
+    license="MIT",
     author='Jean Nassar',
-    author_email='jn.masasin@foo.com',
+    author_email='jn.masasin@gmail.com',
+    url="https://github.com/masasin/autojaki",
     packages=['autojaki'],
+    install_requires=DEPENDENCIES,
 )
